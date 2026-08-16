2ac2d7ba948cd684cc20143b49843a0165a87df7
Indlamuri99 <112086259+Indlamuri99@users.noreply.github.com>
2026-08-16 09:07:11 +0530

    Update backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,14 +1,4 @@
 terraform {
-<<<<<<< HEAD
-  backend "s3" {
-    bucket         = "enterprise-devops-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-=======
-
  backend "s3" {
 
    bucket = "enterprise-devops-terraform-state"
@@ -23,5 +13,3 @@ terraform {
 
  }
 
->>>>>>> 74950808049ba5fe4b83f659248308a2a9fea6e7
-}
